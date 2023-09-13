a21f232993a894b5cf55beb60ea221efb55ca614
Hamza Javaid <humjavaid@gmail.com>
2023-09-13 11:22:05 +0500

    audio call feature
    have some bugs:
    1).Local camera work on every device
    2).The remote user camera doesnot work .

---
+++ lib/credentials.dart
@@ -0,0 +1,6 @@
+class cred{
+
+   double appid = 2119034986;
+  String appSignIN = "a451e8684235f1d4fb3349285f3b5aeed8c696a684f400f5c4393261a3427440";
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:zego_uikit_prebuilt_call/zego_uikit_prebuilt_call.dart';
+import 'dart:math' as math;
+import 'credentials.dart';
 
-
+final String localuserID = math.Random().nextInt(1000).toString();
+cred credd = cred();
 void main(){
-  
+
+  runApp(const MaterialApp(home:CallPage(callID: '12',)));
+}
+
+class CallPage extends StatelessWidget {
+  final String callID;
+  const CallPage({Key? key, required this.callID}) : super(key: key);
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ZegoUIKitPrebuiltCall(
+          appID: credd.appid.toInt(), // Fill in the appID that you get from ZEGOCLOUD Admin Console.
+          appSign: credd.appSignIN, // Fill in the appSign that you get from ZEGOCLOUD Admin Console.
+          userID: localuserID ,
+          userName: "user ${DateTime.now().millisecondsSinceEpoch}",
+          callID: callID,
+          // You can also use groupVideo/groupVoice/oneOnOneVoice to make more types of calls.
+          config: ZegoUIKitPrebuiltCallConfig.oneOnOneVoiceCall()
+            ..onOnlySelfInRoom = (context) {
+            Navigator.pop(context);
+          }
+        ),
+      ),
+    );
+  }
 }
