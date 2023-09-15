bf1e3f2d4cfc723da31fcc76d2dae2b216743707
Hamza Javaid <humjavaid@gmail.com>
2023-09-15 15:41:59 +0500

    Update credentials.dart

--- lib/credentials.dart
+++ lib/credentials.dart
@@ -1,7 +1,7 @@
 class Credentials {
   Credentials._();
 
-  static int appId = 143332319;
+  static int appId = "----use your app ID -------";
   static String appSignIn =
-      "676583a1639e1ead36511de1be65f8b3fbcd366d1597d16efff87abce1bd0947";
+      "----use your app SignIN -------";
 }
